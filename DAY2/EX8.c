9881ddd6d83c8e3797aa2bd89eaae36c1e96e5c4
IlyassMQ <154831683+IlyassMQ@users.noreply.github.com>
2025-09-04 09:32:14 +0100

    EX8.c

--- DAY2/EX8.c
+++ DAY2/EX8.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
-    char mot[50];  
-    int total;   
+    char n[50]; 
 
-    printf("Entrez un mot : ");
-    scanf("%s", mot);  
-    total=0;
-    for (int i = 0; mot[i] != '\0'; i++) {   
-        char c = toupper(mot[i]);            
-        if (c=='A'||c=='E'||c=='I'||c=='L'||c=='N'||c=='O'||c=='R'||c=='S'||c=='T'||c=='U') 
-            total += 1;
-        if (c=='D'||c=='G') 
-            total += 2;
-        if (c=='B'||c=='C'||c=='M'||c=='P') 
-            total += 3;
-        if (c=='F'||c=='H'||c=='V'||c=='W'||c=='Y') 
-            total += 4;
-        if (c=='K') 
-            total += 5;
-        if (c=='J'||c=='X') 
-            total += 8;
-        if (c=='Q'||c=='Z') 
-            total += 10;
+    printf("Entrer le numero de telephone : ");
+    scanf("%s", n);   
+
+    for (int i = 0; n[i] != '\0'; i++) {
+        if (n[i] == 'A' || n[i] == 'B' || n[i] == 'C') {
+            n[i] = '2';
+        }
+        else if (n[i] == 'D' || n[i] == 'E' || n[i] == 'F') {
+            n[i] = '3';
+        }
+        else if (n[i] == 'G' || n[i] == 'H' || n[i] == 'I') {
+            n[i] = '4';
+        }
+        else if (n[i] == 'J' || n[i] == 'K' || n[i] == 'L') {
+            n[i] = '5';
+        }
+        else if (n[i] == 'M' || n[i] == 'N' || n[i] == 'O') {
+            n[i] = '6';
+        }
+        else if (n[i] == 'P' || n[i] == 'R' || n[i] == 'S') {
+            n[i] = '7';
+        }
+        else if (n[i] == 'T' || n[i] == 'U' || n[i] == 'V') {
+            n[i] = '8';
+        }
+        else if (n[i] == 'W' || n[i] == 'X' || n[i] == 'Y') {
+            n[i] = '9';
+        }    
     }
-    
-    printf("Valeur Scrabble : %d\n", total);
+    printf("%s", n);
+
     return 0;
 }
