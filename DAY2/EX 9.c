ef60d70e2db963504fac1e514c2735ac74abc675
IlyassMQ <154831683+IlyassMQ@users.noreply.github.com>
2025-09-04 09:32:55 +0100

    EX 9.c

--- DAY2/EX 9.c
+++ DAY2/EX 9.c
@@ -12,26 +12,20 @@ int main() {
         char c = toupper(mot[i]);            
         if (c=='A'||c=='E'||c=='I'||c=='L'||c=='N'||c=='O'||c=='R'||c=='S'||c=='T'||c=='U') 
             total += 1;
-
         if (c=='D'||c=='G') 
             total += 2;
-
         if (c=='B'||c=='C'||c=='M'||c=='P') 
             total += 3;
-
         if (c=='F'||c=='H'||c=='V'||c=='W'||c=='Y') 
             total += 4;
-
         if (c=='K') 
             total += 5;
-
         if (c=='J'||c=='X') 
             total += 8;
-
         if (c=='Q'||c=='Z') 
             total += 10;
     }
-
-    printf("Valeur Scrabble : %d", total);
+    
+    printf("Valeur Scrabble : %d\n", total);
     return 0;
 }
